02ef87ec0bf9d8c1787801a388006efa31fc4f8e
Mateus Garcia <mateussk83@gmail.com>
2022-09-28 07:59:16 -0300

    install library stitches

--- ignite-shop/src/pages/index.tsx
+++ ignite-shop/src/pages/index.tsx
@@ -1,5 +1,24 @@
+import { styled } from "../styles"
+// bom diferente do styled component só precisamos colocar styled(da biblioteca styles nao do stitches)
+// primeiro parametro -> tipo da tag
+//estruturação igual js entao nomeclatura sem -_ só Maiusculo
+const Button = styled('button', {
+  backgroundColor: '$rocketseat',
+  borderRadius: 4,
+  border: 0,
+  padding: '4px 8px',
+
+  span: {
+    fontWeight: 'bold',
+  },
+
+  '&:hover': {
+    filter: 'brightness(0.8)'
+  }
+})
+
 export default function Home() {
   return (
-    <h1>Hello World!</h1>
+    <Button>Enviar</Button>
   )
 }
