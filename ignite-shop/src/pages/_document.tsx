1e733997797a2878a5c08d2709fab4fbbb5636fd
Mateus Garcia <mateussk83@gmail.com>
2022-09-29 07:39:21 -0300

    configuration global styles

--- ignite-shop/src/pages/_document.tsx
+++ ignite-shop/src/pages/_document.tsx
@@ -1,6 +1,6 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 import { getCssText } from '../styles'
-
+// carrega uma unica vez!! nao colocar css no document
 export default function Document() {
  return(
   <Html>
@@ -8,7 +8,7 @@ export default function Document() {
    <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
-    {/* esta tag serve para renderezar o css no serversizeRender da aplicação resumindo na pagina do next.js */}
+    {/* esta tag serve para renderezar o css no serversizeRender da aplicação resumindo na pagina do next.js tambem fica mais rapido porque o server do next ja vai estar com o carregamento em maos do css */}
     <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }} />   
    </Head>
    <body>
--- ignite-shop/src/pages/index.tsx
+++ ignite-shop/src/pages/index.tsx
@@ -3,7 +3,7 @@ import { styled } from "../styles"
 // primeiro parametro -> tipo da tag
 //estruturação igual js entao nomeclatura sem -_ só Maiusculo
 const Button = styled('button', {
-  backgroundColor: '$rocketseat',
+  backgroundColor: '$green300',
   borderRadius: 4,
   border: 0,
   padding: '4px 8px',
