565874d30128fdd1b8573316b20be11bce9535d8
Mateus Garcia <mateussk83@gmail.com>
2023-03-08 19:53:18 -0300

    feat insert stitches

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    optimizeCss: true, // enabling this will enable SSR for Tailwind
+  },
+
   reactStrictMode: true,
 }
 
---
+++ src/pages/_app.tsx
@@ -0,0 +1,22 @@
+import type { AppProps } from 'next/app'
+import { globalStyles } from '../styles/global';
+
+import logoImg from "../assets/logo.svg"
+import { Container, Header } from '../styles/pages/app';
+
+
+
+export default function App({ Component, pageProps }: AppProps) {
+  globalStyles();
+
+  return (
+    <Container>
+
+      <Header>
+        <img src={logoImg.src} alt="" />
+      </Header>
+      <Component {...pageProps} />
+
+    </Container>
+  )
+}
